1712dfbbf17a96736ff7b7b4292f21f68ce32b6a
Shiyu Liu <Shiyul4@illinois.edu>
2024-12-14 20:53:18 -0600

    Added new files to submit1

---
+++ activity_recognition_demo.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36744059c13255f8b2aed44dd5d1245aa62643e68462c91554cf791a4971af63
+size 2709
---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5df707b8544b2107b1bbe4d5659ebb71b28f2a38616ec37735fd517360e641c
+size 5402
